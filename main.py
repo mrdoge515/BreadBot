62ac4b56e07982179d6970d82b1ed2028ca5f3ce
Dawid <81042299+MrDoge00@users.noreply.github.com>
2022-02-04 21:52:53 +0000

    Minor changes

--- main.py
+++ main.py
@@ -3,16 +3,19 @@ import discord
 from discord.ext import commands
 from keep_alive import keep_alive
 
-maintenance = 1
+maintenance = 0
 
+# class containing all events and commands
 class MyClient(discord.Client):
   
+  # thing bot do on boot
   async def on_ready(self):
     activity = discord.Game(name="Bread Baking Simulator", type=3)
     await client.change_presence(status=discord.Status.idle, activity=activity)
     print('[System] Logged on as', self.user)
     print('[System] Running version 1.0')
 
+  # replying to specific messages
   async def on_message(self, message):
     if message.author == self.user:
       return
@@ -22,6 +25,10 @@ class MyClient(discord.Client):
       print('[Event handler] "Bread 👍" event handled')
 
     if message.content == "LOL":
+      await message.channel.send(file=discord.File('gif/cereal_dude.gif'))
+      print('[Event handler] "LOL" event handled')
+
+    if message.content == "lol":
       await message.channel.send(file=discord.File('gif/cereal_dude.gif'))
       print('[Event handler] "LOL" event handled')
 
@@ -45,22 +52,26 @@ class MyClient(discord.Client):
       await message.channel.send(embed=embed)
       print('[Event handler] "Realtek17" event handled')
 
-class MyClientMeintanance(discord.Client):
+# class for maintenance mode
+class MyClientMaintanance(discord.Client):
   async def on_ready(self):
     activity = discord.Game(name="Bot under maintenance", type=3)
     await client.change_presence(status=discord.Status.idle, activity=activity)
     print('[System] Logged on as', self.user)
     print('[System] Running version 1.0')
+    print('[System] Maintenance mode')
 
 
-
+# checking if maintenance status is enabled
 if(maintenance == 1):
-  client = MyClientMeintanance()
+  client = MyClientMaintanance()
 elif(maintenance == 0):
   client = MyClient()
 else:
   print('[System] Maintenance variable error')
   exit()
 
+# function allowing the bot to be hosted
 keep_alive()
+# starting the bot
 client.run(os.getenv('TOKEN'))
