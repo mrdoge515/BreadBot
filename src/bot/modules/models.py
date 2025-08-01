6870dbe64ca21b171cb712a48ebcfafa3d91c572
mrdoge515 <mrdogerrr@gmail.com>
2025-08-01 17:13:58 +0200

    fix: int overflow when using mysql

--- src/bot/modules/models.py
+++ src/bot/modules/models.py
@@ -1,11 +1,12 @@
-from sqlmodel import SQLModel, Field
+from sqlalchemy import BigInteger
+from sqlmodel import SQLModel, Column, Field
 
 
 class User(SQLModel, table=True):
     __table_args__ = {"extend_existing": True}
 
     id: int | None = Field(default=None, primary_key=True)
-    user_id: int = Field(index=True)
-    guild_id: int = Field()
+    user_id: int = Field(sa_column=Column(BigInteger))
+    guild_id: int = Field(sa_column=Column(BigInteger))
     message_count: int = Field(default=0)
     
