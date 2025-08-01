37685e5dc160793eb1ca6e0deac43d174bd212ac
mrdoge515 <mrdogerrr@gmail.com>
2025-08-01 22:00:06 +0200

    feat: add data models for handling reminders

--- src/bot/modules/models.py
+++ src/bot/modules/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from sqlalchemy import BigInteger
 from sqlmodel import SQLModel, Column, Field
 
@@ -9,4 +11,18 @@ class User(SQLModel, table=True):
     user_id: int = Field(sa_column=Column(BigInteger))
     guild_id: int = Field(sa_column=Column(BigInteger))
     message_count: int = Field(default=0)
-    
+
+class User_Timezone(SQLModel, table=True):
+    __table_args__ = {"extend_existing": True}
+
+    id: int | None = Field(default=None, primary_key=True)
+    user_id: int = Field(sa_column=Column(BigInteger))
+    timezone: str = Field(default="UTC")
+
+class Reminder(SQLModel, table=True):
+    __table_args__ = {"extend_existing": True}
+
+    id: int | None = Field(default=None, primary_key=True)
+    user_id: int = Field(sa_column=Column(BigInteger))
+    time: datetime.datetime = Field(nullable=False)
+    text: str = Field()
