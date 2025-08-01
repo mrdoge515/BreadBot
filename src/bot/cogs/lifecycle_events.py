0c07a152f81721fd6b18e389a50447d4a383dffe
mrdoge515 <mrdogerrr@gmail.com>
2025-08-01 19:56:27 +0200

    feat: sync slash commands on startup

--- src/bot/cogs/lifecycle_events.py
+++ src/bot/cogs/lifecycle_events.py
@@ -9,6 +9,7 @@ class Lifecycle_Events(commands.Cog):
     
     @commands.Cog.listener()
     async def on_ready(self):
+        await self.bot.tree.sync()
         activity = discord.Activity(type=discord.ActivityType.watching, name="the oven")
         await self.bot.change_presence(activity=activity)
         print(f"{colorama.Fore.GREEN}[✓]{colorama.Fore.RESET} {self.bot.user} just went online!")
