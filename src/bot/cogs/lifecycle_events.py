ab3d4cdc9e0fec245353441dc1656f2daa68d892
mrdoge515 <mrdogerrr@gmail.com>
2025-08-01 14:33:31 +0200

    feat: add an on_ready listener

---
+++ src/bot/cogs/lifecycle_events.py
@@ -0,0 +1,17 @@
+import colorama
+import discord
+from discord.ext import commands
+
+
+class Lifecycle_Events(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        activity = discord.Activity(type=discord.ActivityType.watching, name="the oven")
+        await self.bot.change_presence(activity=activity)
+        print(f"{colorama.Fore.GREEN}[✓]{colorama.Fore.RESET} {self.bot.user} just went online!")
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Lifecycle_Events(bot)) 
